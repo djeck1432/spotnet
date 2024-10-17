05b56765ed24bf5690af44448cc7e5541621bc78
djeck1432 <oleh.kostashchuk@gmail.com>
2024-10-17 20:31:06 +0200

    harcoded dashboard

--- web_app/api/dashboard.py
+++ web_app/api/dashboard.py
@@ -1,4 +1,5 @@
 import collections
+from datetime import datetime
 
 from fastapi import APIRouter
 from starlette.requests import Request
@@ -18,10 +19,12 @@ async def get_dashboard(request: Request, wallet_id: str) -> dict:
     :param request: HTTP request
     :return: template response
     """
-    contract_address = position_db_connector.get_contract_address_by_wallet_id(wallet_id)
-    opened_positions = position_db_connector.get_positions_by_wallet_id(wallet_id)
+    # FIXME
+    wallet_id = "0x27994c503bd8c32525fbdaf9d398bdd4e86757988c64581b055a06c5955ea49"
+    contract_address = "0x698b63df00be56ba39447c9b9ca576ffd0edba0526d98b3e8e4a902ffcf12f0" # position_db_connector.get_contract_address_by_wallet_id(wallet_id)
+    # opened_positions = position_db_connector.get_positions_by_wallet_id(wallet_id)
 
-    first_opened_position = opened_positions[0] if opened_positions else collections.defaultdict(lambda: None)
+    #first_opened_position = opened_positions[0] if opened_positions else collections.defaultdict(lambda: None)
     # Fetch zkLend position for the wallet ID
     zklend_position = await DashboardMixin.get_zklend_position(contract_address)
 
@@ -29,7 +32,7 @@ async def get_dashboard(request: Request, wallet_id: str) -> dict:
     wallet_balances = await DashboardMixin.get_wallet_balances(wallet_id)
     return {
         "balances": wallet_balances,
-        "multipliers": {"ETH": first_opened_position["multiplier"]},
-        "start_dates": {"ETH": first_opened_position["created_at"]},
+        "multipliers": {"ETH": 3}, #first_opened_position["multiplier"]},
+        "start_dates": {"ETH": datetime.now().isoformat()}, # first_opened_position["created_at"]},
         "zklend_position": zklend_position,
     }
