2bd0f03f29d5024748f3c3ea991241ee00089059
MaryammAli <maryamaliyu09159@gmail.com>
2025-02-25 07:57:01 +0100

    fix changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import AppRouter from './app/router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppRouter>
-      <App />
-      <TanStackRouterDevtools initialIsOpen={false} position="bottom-right" />
-    </AppRouter>
+    <App />
   </React.StrictMode>
 );
