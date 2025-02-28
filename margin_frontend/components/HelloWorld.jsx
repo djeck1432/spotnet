2344e952ee88b3192030fa2a96ee78ccb1ccefe5
enyinnaya1234 <chemicalnilton@gmail.com>
2025-02-28 21:58:45 +0100

    chore: [MarginFrontend] Fix linter & build

--- margin_frontend/components/HelloWorld.jsx
+++ margin_frontend/components/HelloWorld.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 
 export default function HelloWorld({ name }) {
-  const [count, setCount] = useState(1);
-  return (
-    <div>
-      <h1>
-        Hello {name} x{count}!
-      </h1>
-      <button onClick={() => setCount((c) => c + 1)}>Increment</button>
-    </div>
-  );
+	const [count, setCount] = useState(1);
+	return (
+		<div>
+			<h1>
+				Hello {name} x{count}!
+			</h1>
+			<button type="button" onClick={() => setCount((c) => c + 1)}>
+				Increment
+			</button>
+		</div>
+	);
 }
